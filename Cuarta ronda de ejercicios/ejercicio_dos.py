b4749b1b7ff2fb8fd98fd31e6c8ef18d7904dc48
meiphi <malena.cadima@gmail.com>
2020-09-22 10:32:26 -0300

    Se agregaron los archivos en la carpeta de cuarta ronda de ejercicios: ejercicio_uno.py, ejercicio_dos.py

---
+++ Cuarta ronda de ejercicios/ejercicio_dos.py
@@ -0,0 +1,10 @@
+'''
+- Crear una clase llamada Rectángulo con dos puntos (inicial y final) que formarán la diagonal del rectángulo.
+'''
+
+class Rectangulo(object):
+	"""docstring for Rectangulo"""
+	def __init__(self, arg):
+		super(Rectangulo, self).__init__()
+		self.arg = arg
+		
---
+++ Cuarta ronda de ejercicios/ejercicio_uno.py
@@ -0,0 +1,39 @@
+import math
+
+class Punto:
+	def __init__(self, x = 0, y = 0):
+		self.x = x
+		self.y = y
+
+	def __str__(self):
+		print(f'({self.x}, {self.y})')
+
+	def cuadrante(self):
+		if self.x == 0 and self.y != 0:
+			print('se sitúa sobre el eje Y')
+		elif self.x != 0 and self.y == 0:
+			print('se sitúa sobre el eje X')
+		elif self.x == 0 and self.y == 0:
+			print('está sobre el origen')
+
+	def vector(self, p):
+		vector = (p.x - self.x  ,  p.y - self.x)
+		print(vector)
+
+	def distancia(self, p):
+		d = math.sqrt( pow(self.y - self.x, 2) + pow(p.y - p.x, 2) )
+		print(f'la distancia es {d}')
+
+A = Punto(2,3)
+B = Punto(5,5)
+C = Punto(-3, -1)
+D = Punto(0,0)
+
+A.vector(B)
+C.vector(D)
+
+D.cuadrante()
+
+# A(2, 3), B(5,5), C(-3, -1) y D(0,0)
+
+		
